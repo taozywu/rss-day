834e0de4e2220275affbac5b0a9a7578a97b6211
taozywu <taozy.wu@qq.com>
2021-10-19 12:04:16 +0800

    m

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def replace_readme():
             link = before_info
             # 生成超链接
             rss_info = rss_info_list[index]
-            wx_content = ""
+            #wx_content = ""
             parse_result = urlparse(link)
             scheme_netloc_url = str(parse_result.scheme)+"://"+str(parse_result.netloc)
 
@@ -146,12 +146,12 @@ def replace_readme():
                 for rss_info_atom in rss_info:
                     if (rss_info_atom["date"] == datetime.today().strftime("%Y-%m-%d")):
                         if new_num > 0 and new_num % 14 == 0:
-                            send_qyweixin(wx_content)
-                            wx_content = ""
+                            #send_qyweixin(wx_content)
+                            #wx_content = ""
                          
-                        wx_content = wx_content + rss_info_atom["title"] + rss_info_atom["link"] + "<br/>\n"
+                        #wx_content = wx_content + rss_info_atom["title"] + rss_info_atom["link"] + "<br/>\n"
                         new_num = new_num + 1
-                        current_date_news_index[0] = current_date_news_index[0] + rss_info_atom["title"] + rss_info_atom["link"] + "<br/>\n"
+                        current_date_news_index[0] = current_date_news_index[0] + "<a href='"+rss_info_atom["link"]+"'>"+rss_info_atom["title"]+ "</a><br/>\n"
 
             except:
                 print("An exception occurred")
@@ -209,7 +209,7 @@ def main():
     reResult = re.findall(mail_re, readme_md[0])
 
     try:
-        send_mail(email_list, "Rss", readme_md[1])
+        send_mail(email_list, "Rss day", readme_md[1])
     except Exception as e:
         print("==发送邮件失败==》》", e)
 

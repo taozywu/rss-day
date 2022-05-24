a508b7960875670fc242d39b4c5fb127b97e974e
taozywu <taozywu@users.noreply.github.com>
2022-05-24 23:09:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def replace_readme():
             link = before_info
             # 生成超链接
             rss_info = rss_info_list[index]
-            wx_content = ""
+            #wx_content = ""
             parse_result = urlparse(link)
             scheme_netloc_url = str(parse_result.scheme)+"://"+str(parse_result.netloc)
 
@@ -145,12 +145,12 @@ def replace_readme():
             try:
                 for rss_info_atom in rss_info:
                     if (rss_info_atom["date"] == datetime.today().strftime("%Y-%m-%d")):
-                        if new_num > 0 and new_num % 14 == 0:
-                            send_qyweixin(wx_content)
-                            wx_content = ""
+                        #if new_num > 0 and new_num % 14 == 0:
+                            #send_qyweixin(wx_content)
+                            #wx_content = ""
                          
-                        wx_content = wx_content + rss_info_atom["title"] + rss_info_atom["link"] + "<br/>\n"
-                        new_num = new_num + 1
+                        #wx_content = wx_content + rss_info_atom["title"] + rss_info_atom["link"] + "<br/>\n"
+                        #new_num = new_num + 1
                         current_date_news_index[0] = current_date_news_index[0] + "<a href='"+rss_info_atom["link"]+"'>"+rss_info_atom["title"]+ "</a><br/>\n"
 
             except:
